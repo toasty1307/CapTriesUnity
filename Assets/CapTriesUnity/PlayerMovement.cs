30b05136c1b6b636ee9cbf10d10b3bd462327b68
toasty <sharmagarvit614@gmail.com>
2021-09-22 19:43:12 +0530

    pusj

--- Assets/CapTriesUnity/PlayerMovement.cs
+++ Assets/CapTriesUnity/PlayerMovement.cs
@@ -1,4 +1,3 @@
-using System;
 using InputActions;
 using UnityEngine;
 
@@ -7,49 +6,91 @@ namespace CapTriesUnity
     [RequireComponent(typeof(Rigidbody))]
     public class PlayerMovement : MonoBehaviour
     {
-        private PlayerActions InputAction { get; set; }
-        private Vector2 PlayerInput { get; set; }
-        private Rigidbody Rigidbody { get; set; }
+        private PlayerActions _inputAction;
+        private Vector2 _moveInput;
+        private Vector2 _mouseInput;
+        private Rigidbody _rigidbody;
+        private Rigidbody _camRigidbody;
+        private Transform _camTransform;
+        private bool _sprinting;
         
-        [field: SerializeField] private Vector2 MovementSpeed { get; set; }
-        
+        [Header("Camera")] 
+        [SerializeField] private Camera playerCamera;
+        [SerializeField] private bool invertY;
+        [SerializeField] private Vector2 mouseSensitivity;
+        [SerializeField, Range(-180, 180)] private float yClampMin;
+        [SerializeField, Range(-180, 180)] private float yClampMax;
+        [SerializeField, Range(0f, 1f)] private float axisSmoothing;
+
+        [Header("Movement")]
+        [SerializeField] private Vector2 accelerationAmount;
+        [SerializeField] private float torqueAmount;
+        [SerializeField, Range(0f, 1f)] private float counterMovement;
+        [SerializeField, Range(1, 3f)] private float sprintMultiplier;
+
+
         private void Awake()
         {
-            InputAction = new PlayerActions();
-            Rigidbody = GetComponent<Rigidbody>();
+            _inputAction = new PlayerActions();
+            _rigidbody = GetComponent<Rigidbody>();
+            _camRigidbody = playerCamera.GetComponent<Rigidbody>();
+            _camTransform = playerCamera.transform;
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
         }
 
         private void FixedUpdate()
         {
             GetInput();
+            CameraStuff();
             ApplyMovement();
         }
 
-        public void ApplyMovement()
+        private void CameraStuff()
         {
-            var vec3 = Vector3.zero;
-            var currentPos = transform.position;
-            vec3.x = PlayerInput.x;
-            vec3.z = PlayerInput.y;
-            var destination = currentPos + vec3;
-            destination *= Time.fixedDeltaTime;
-            destination.y = currentPos.y;
-            Rigidbody.MovePosition(destination);
+            var smoothingModifier = 1 - axisSmoothing;
+            _rigidbody.AddTorque(-_rigidbody.angularVelocity * smoothingModifier, ForceMode.VelocityChange);
+            _rigidbody.AddTorque(Vector3.up * (_mouseInput.x * torqueAmount * smoothingModifier), ForceMode.VelocityChange);
+            _camRigidbody.AddTorque(-_camRigidbody.angularVelocity * smoothingModifier, ForceMode.VelocityChange);
+            _camRigidbody.AddTorque(_camTransform.right * (_mouseInput.y * torqueAmount * (invertY ? -1 : 1) * smoothingModifier), ForceMode.VelocityChange);
+            var camEulerAngles = _camTransform.eulerAngles;
+            camEulerAngles.y = _rigidbody.transform.eulerAngles.y;
+            camEulerAngles.z = 0;
+            _camTransform.eulerAngles = camEulerAngles;
         }
 
-        public void GetInput()
+        private void ApplyMovement()
         {
-            PlayerInput = InputAction.Movement.Movement.ReadValue<Vector2>() * MovementSpeed;
+            var transform1 = transform;
+            var velocity = transform1.forward * _moveInput.y + transform1.right * _moveInput.x;
+            velocity *= _sprinting ? sprintMultiplier : 1f;
+            /*velocity.y = _rigidbody.velocity.y;*/
+            if (_moveInput == Vector2.zero)
+                _rigidbody.AddForce(-_rigidbody.velocity * counterMovement, ForceMode.VelocityChange);
+            else
+                _rigidbody.velocity = Vector3.MoveTowards(_rigidbody.velocity, velocity, accelerationAmount.magnitude * Time.fixedDeltaTime);
+        }
+
+        private void GetInput()
+        {
+            _sprinting = _inputAction.Movement.Run.ReadValue<float>() != 0;
+            _moveInput = _inputAction.Movement.Movement.ReadValue<Vector2>() * (Time.fixedDeltaTime * accelerationAmount);
+            _mouseInput = _inputAction.Mouse.Look.ReadValue<Vector2>() * (Time.fixedDeltaTime * mouseSensitivity);
         }
 
         private void OnEnable()
         {
-            InputAction.Enable();
+            _inputAction.Enable();
         }
 
         private void OnDisable()
         {
-            InputAction.Disable();
+            _inputAction.Disable();
+        }
+
+        private void OnValidate()
+        {
+            _camRigidbody = playerCamera.GetComponent<Rigidbody>();
         }
     }
 }
--- Assets/InputActions/PlayerActions.cs
+++ Assets/InputActions/PlayerActions.cs
@@ -35,6 +35,14 @@ namespace InputActions
                     ""expectedControlType"": ""Button"",
                     ""processors"": """",
                     ""interactions"": """"
+                },
+                {
+                    ""name"": ""Run"",
+                    ""type"": ""Button"",
+                    ""id"": ""272e6193-d3dc-45d0-a398-b4a5845d8962"",
+                    ""expectedControlType"": ""Button"",
+                    ""processors"": """",
+                    ""interactions"": ""Hold""
                 }
             ],
             ""bindings"": [
@@ -158,6 +166,44 @@ namespace InputActions
                     ""action"": ""Jump"",
                     ""isComposite"": false,
                     ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""42b492ed-99c5-4e38-a917-bff34e3888d5"",
+                    ""path"": ""<Keyboard>/leftShift"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": ""PCMasterRace"",
+                    ""action"": ""Run"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                }
+            ]
+        },
+        {
+            ""name"": ""Mouse"",
+            ""id"": ""303cb78d-7be9-4819-80fc-f230abfff657"",
+            ""actions"": [
+                {
+                    ""name"": ""Look"",
+                    ""type"": ""Value"",
+                    ""id"": ""f477e0d7-6abf-4cc9-b806-3efb5b0c8ba6"",
+                    ""expectedControlType"": ""Vector2"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                }
+            ],
+            ""bindings"": [
+                {
+                    ""name"": """",
+                    ""id"": ""fadb0ec3-b78a-4ca4-b9bc-ff628a1b68bc"",
+                    ""path"": ""<Mouse>/delta"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": ""PCMasterRace"",
+                    ""action"": ""Look"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
                 }
             ]
         }
@@ -185,6 +231,10 @@ namespace InputActions
             m_Movement = asset.FindActionMap("Movement", throwIfNotFound: true);
             m_Movement_Movement = m_Movement.FindAction("Movement", throwIfNotFound: true);
             m_Movement_Jump = m_Movement.FindAction("Jump", throwIfNotFound: true);
+            m_Movement_Run = m_Movement.FindAction("Run", throwIfNotFound: true);
+            // Mouse
+            m_Mouse = asset.FindActionMap("Mouse", throwIfNotFound: true);
+            m_Mouse_Look = m_Mouse.FindAction("Look", throwIfNotFound: true);
         }
 
         public void Dispose()
@@ -236,12 +286,14 @@ namespace InputActions
         private IMovementActions m_MovementActionsCallbackInterface;
         private readonly InputAction m_Movement_Movement;
         private readonly InputAction m_Movement_Jump;
+        private readonly InputAction m_Movement_Run;
         public struct MovementActions
         {
             private @PlayerActions m_Wrapper;
             public MovementActions(@PlayerActions wrapper) { m_Wrapper = wrapper; }
             public InputAction @Movement => m_Wrapper.m_Movement_Movement;
             public InputAction @Jump => m_Wrapper.m_Movement_Jump;
+            public InputAction @Run => m_Wrapper.m_Movement_Run;
             public InputActionMap Get() { return m_Wrapper.m_Movement; }
             public void Enable() { Get().Enable(); }
             public void Disable() { Get().Disable(); }
@@ -257,6 +309,9 @@ namespace InputActions
                     @Jump.started -= m_Wrapper.m_MovementActionsCallbackInterface.OnJump;
                     @Jump.performed -= m_Wrapper.m_MovementActionsCallbackInterface.OnJump;
                     @Jump.canceled -= m_Wrapper.m_MovementActionsCallbackInterface.OnJump;
+                    @Run.started -= m_Wrapper.m_MovementActionsCallbackInterface.OnRun;
+                    @Run.performed -= m_Wrapper.m_MovementActionsCallbackInterface.OnRun;
+                    @Run.canceled -= m_Wrapper.m_MovementActionsCallbackInterface.OnRun;
                 }
                 m_Wrapper.m_MovementActionsCallbackInterface = instance;
                 if (instance != null)
@@ -267,10 +322,46 @@ namespace InputActions
                     @Jump.started += instance.OnJump;
                     @Jump.performed += instance.OnJump;
                     @Jump.canceled += instance.OnJump;
+                    @Run.started += instance.OnRun;
+                    @Run.performed += instance.OnRun;
+                    @Run.canceled += instance.OnRun;
                 }
             }
         }
         public MovementActions @Movement => new MovementActions(this);
+
+        // Mouse
+        private readonly InputActionMap m_Mouse;
+        private IMouseActions m_MouseActionsCallbackInterface;
+        private readonly InputAction m_Mouse_Look;
+        public struct MouseActions
+        {
+            private @PlayerActions m_Wrapper;
+            public MouseActions(@PlayerActions wrapper) { m_Wrapper = wrapper; }
+            public InputAction @Look => m_Wrapper.m_Mouse_Look;
+            public InputActionMap Get() { return m_Wrapper.m_Mouse; }
+            public void Enable() { Get().Enable(); }
+            public void Disable() { Get().Disable(); }
+            public bool enabled => Get().enabled;
+            public static implicit operator InputActionMap(MouseActions set) { return set.Get(); }
+            public void SetCallbacks(IMouseActions instance)
+            {
+                if (m_Wrapper.m_MouseActionsCallbackInterface != null)
+                {
+                    @Look.started -= m_Wrapper.m_MouseActionsCallbackInterface.OnLook;
+                    @Look.performed -= m_Wrapper.m_MouseActionsCallbackInterface.OnLook;
+                    @Look.canceled -= m_Wrapper.m_MouseActionsCallbackInterface.OnLook;
+                }
+                m_Wrapper.m_MouseActionsCallbackInterface = instance;
+                if (instance != null)
+                {
+                    @Look.started += instance.OnLook;
+                    @Look.performed += instance.OnLook;
+                    @Look.canceled += instance.OnLook;
+                }
+            }
+        }
+        public MouseActions @Mouse => new MouseActions(this);
         private int m_PCMasterRaceSchemeIndex = -1;
         public InputControlScheme PCMasterRaceScheme
         {
@@ -284,6 +375,11 @@ namespace InputActions
         {
             void OnMovement(InputAction.CallbackContext context);
             void OnJump(InputAction.CallbackContext context);
+            void OnRun(InputAction.CallbackContext context);
+        }
+        public interface IMouseActions
+        {
+            void OnLook(InputAction.CallbackContext context);
         }
     }
 }
